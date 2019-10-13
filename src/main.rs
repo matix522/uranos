1149c14bd7ca9212cc9c388f662fa9a3a3c97b13
= <=>
2019-10-13 23:15:54 +0200

    Main cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ pub mod time;
 pub mod interupt;
 pub mod sync;
 
+use aarch64::*;
+
 #[cfg(not(feature = "raspi4"))]
 const MMIO_BASE: u32 = 0x3F00_0000;
 #[cfg(feature = "raspi4")]
@@ -19,49 +21,31 @@ const MMIO_BASE: u32 = 0xFE00_0000;
 
 extern "C" {
     pub fn _boot_cores() -> !;
+    static mut __binary_end : u64;
 }
 
 fn kernel_entry() -> ! {
     let mut mbox = mbox::Mbox::new();
     let uart = uart::Uart::new();
-    // let u = uart::Uart;
 
-    // u.init(&mut mbox);
-    // u.puts("PRE Mutex");
-    // let uart = io::UART.lock();
-        
-    //set up serial console
     match uart.init(&mut mbox) {
-        Ok(_) =>  uart.puts("\n[0] UART is live!\n"),
-        Err(_) => loop {
-             unsafe { 
-                asm!("wfe" :::: "volatile") 
-                 }; // If UART fails, abort early
-        },
+        Ok(_) =>  println!("[ OK ] Uart Loaded"),
+        Err(_) => halt()
     }
     
-    uart.puts("HELLO PIOTREK");
-    uart.getc();
-    uart.puts("\n\rXDDDDD");
-    // println!("{:?} {:?}", 5, 10);
+    println!("Exception Level: {:?}", boot::mode::ExceptionLevel::get_current());
 
-    // println!("Exception Level: {:?}", boot::mode::ExceptionLevel::get_current());
-    // // echo everything back
+    println!("Binary loaded at: {:x} ", _boot_cores as *const () as u64 );
+
+    println!("Binary ends at: {:x} ", unsafe { __binary_end } );
+
+    println!("Kernel Initialization complete.");
+
+    // echo everything back
     
-
-
-    unsafe{
-
-        println!("Boot cores: {:x} ", _boot_cores as *const () as u64 );
-        println!("reset: {:x} ",boot::reset as *const () as u64 );
-        println!("kernel: {:x} ",kernel_entry as *const () as u64 );
-    }
     loop { 
         uart.send(uart.getc());
     }
 }
-// pub unsafe fn kernel_entry() -> ! {
-//     gpio::setup();
-//     gpio::blink();
-// }
+
 boot::entry!(kernel_entry);
